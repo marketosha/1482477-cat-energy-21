b8bb81a0fc9149fab0cf47e1af321369682f619a
Evgeniya <kabanovae30@gmail.com>
2020-12-27 12:42:47 +0300

    Изменение навигации и исправление по комментариям

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,8 +1,8 @@
-const headerToggle= document.querySelector(".page-header__toogle");
-const headerNavigation = document.querySelector(".page-header");
-headerNavigation.classList.remove('.page-header--nojs');
+  var navMain = document.querySelector('.page-header');
+  var navToggle = document.querySelector('.page-header__toogle');
 
-headerToggle.addEventListener("click" ,function(evt){
-  evt.preventDefault();
-  headerNavigation.classList.toggle("page-header--opened");
-});
+  navMain.classList.remove('page-header--nojs');
+
+  navToggle.onclick = function() {
+  navMain.classList.toggle('page-header--opened');
+  };
