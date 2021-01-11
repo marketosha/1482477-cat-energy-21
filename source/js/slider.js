65786b8dc4de5c4999d53924d39ec6fcdf8896e6
Evgeniya <kabanovae30@gmail.com>
2021-01-11 21:05:26 +0300

    Исправление слайдера

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,13 +1,13 @@
-var root = document.querySelector(':root');
+var root = document.querySelector(":root");
 var leftButton = document.querySelector(".slider-filter__button--before");
 var rightButton = document.querySelector(".slider-filter__button--after");
 
 leftButton.addEventListener("click", function () {
-  root.style.setProperty('--slide-shift', '0px');
-  root.style.setProperty('--range-position', 'var(--range-position-left)');
+  root.style.setProperty("--slide-shift", "0px");
+  root.style.setProperty("--range-position", "var(--range-position-left)");
 });
 
 rightButton.addEventListener("click", function () {
-  root.style.setProperty('--slide-shift', 'var(--slide-width)');
-  root.style.setProperty('--range-position', 'var(--range-position-right)');
+  root.style.setProperty("--slide-shift", "var(--slide-width)");
+  root.style.setProperty("--range-position", "var(--range-position-right)");
 });
