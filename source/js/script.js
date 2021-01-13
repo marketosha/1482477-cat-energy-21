8384afae281fa2d33b39db7bc883ee068f95eede
Evgeniya <kabanovae30@gmail.com>
2021-01-13 19:13:22 +0300

    Оптимизация

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,14 @@
+var navMain = document.querySelector(".page-header");
+var navToggle = document.querySelector(".page-header__toggle");
+
+navMain.classList.remove("page-header--nojs");
+
+navToggle.onclick = function () {
+  navMain.classList.toggle("page-header--opened");
+};
+
+//Slider
+
 var root = document.querySelector(":root");
 var leftButton = document.querySelector(".slider-filter__button--before");
 var rightButton = document.querySelector(".slider-filter__button--after");
