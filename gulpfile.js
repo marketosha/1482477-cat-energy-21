ccd5b1606ca46bb55788f1783b6a03d2b13b75ab
Evgeniya <kabanovae30@gmail.com>
2021-01-15 16:06:12 +0300

    Исправление в watch

--- gulpfile.js
+++ gulpfile.js
@@ -137,7 +137,7 @@ const reload = done => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.sass", gulp.series(styles));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles, reload));
   gulp.watch("source/js/script.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
